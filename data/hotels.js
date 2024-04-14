8e4d130d348963fc58d685638ad1c68057996b76
Jordon Draper <jordondraper17@gmail.com>
2024-04-13 22:41:31 -0600

    let's check it out

---
+++ data/hotels.js
@@ -0,0 +1,52 @@
+
+export const hotels =
+[
+    {
+      "name":"Passalacqua Lake Como",
+      "photo":"passalacqua.webp",
+      "address":["Via Besana, 59, 22010","Moltrasio CO, Italy"],
+      "phone":"+39 031 144 311"
+    },
+    {
+      "name":"Rosewood Hong Kong",
+      "photo":"rosewood.webp",
+      "address":["19 Salisbury Rd","Tsim Sha Tsui, Hong Kong"],
+      "phone":"+852 3891 888"
+    },
+    {
+      "name":"Soneva Fushi Maldives",
+      "photo":"soneva.webp",
+      "address":["Kunfunadhoo Island","Eydhafushi, Maldives"],
+      "phone":"+960 660-0304"
+    },
+    {
+      "name":"The Calile Australia",
+      "photo":"calile.webp",
+      "address":["48 James St","Fortitude Valley QLD 4006, Australia"],
+      "phone":"+61 7 3607 5888"
+    },
+    {
+      "name":"Claridge's in London",
+      "photo":"claridges.webp",
+      "address":["Brook St, London","W1k 4HR, United Kingdom"],
+      "phone":"+44 20 7629 8860"
+    },
+    {
+      "name":"Raffles in Singapore",
+      "photo":"raffles.webp",
+      "address":["1 Beach Rd","Singapore 189673"],
+      "phone":"+65 6337 1886"
+    },
+    {
+      "name":"Hotel Esencia Tulum",
+      "photo":"esencia.webp",
+      "address":["77734 Xpu Há","Quintana Roo, Mexico"],
+      "phone":"+52 984 873 4830"
+    },
+    {
+      "name":"Royal Mansour",
+      "photo":"royal.webp",
+      "address":["Rue Abou Abbas El Sebti","40000, Morocco"],
+      "phone":"+212 5298-08080"
+    }
+    ]
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,39 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+const hb = document.querySelector('#hamburgerBtn');
+const pw = document.querySelector('#pageWrapper');
+hb.addEventListener('click', () => {
+    pw.classList.toggle('moveOver');
+})
+
+import { hotels } from '../data/hotels.js';
+console.log(hotels);
+const cards=document.querySelector('#fancyhotel')
+
+hotels.forEach(
+ hotel => {
+    const mySection = document.createElement('section')
+
+    const theImage = document.createElement("img")
+    theImage.src = `images_hotel/${hotel.photo}`
+    theImage.alt = `Hotel Name ${hotel.name}`
+
+    const theName = document.createElement("h2")
+    theName.textContent = hotel.name
+
+    const theAddress = document.createElement("p");
+    theAddress.className="address"
+    theAddress.innerHTML = `${hotel.address[0]}<br>${hotel.address[1]}`
+
+    const thePhone = document.createElement("p2");
+    thePhone.textContent = hotel.phone
+
+    mySection.appendChild(theImage)
+    mySection.appendChild(theName)
+    mySection.appendChild(theAddress)
+    mySection.appendChild(thePhone)
+    cards.appendChild(mySection)
+ }   
+);
